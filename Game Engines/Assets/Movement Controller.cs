4fe8390c39c814e2845f444bfc94ef7d1f277d2a
Anthony <Sublethalrex@gmail.com>
2019-09-24 15:23:30 -0700

    CHaracter controller

---
+++ Game Engines/Assets/Movement Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+
+{
+
+    public Animator anim;
+    public float speed = 3.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (anim == null) anim = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
