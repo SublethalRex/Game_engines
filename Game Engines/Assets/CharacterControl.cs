95f9e9a932eea368c87d9bc5b690369e5ddcf060
Anthony <Sublethalrex@gmail.com>
2019-09-30 12:57:55 -0700

    more work on character controller

--- Game Engines/Assets/CharacterControl.cs
+++ Game Engines/Assets/CharacterControl.cs
@@ -6,25 +6,65 @@ public class CharacterControl : MonoBehaviour
 {
 
 
+    public Animator anim;
+    public float speed = 3.5f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (anim == null) anim = GetComponent<Animator>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetButton("w"))
+        if (Input.GetKey("w"))
+        {
+            anim.SetFloat(name: "Verticle", speed);
+
+        }
+        else if (Input.GetKey("s"))
         {
 
+            anim.SetFloat("Verticle", -speed);
+
+        }
+        else
+        {
+
+            anim.SetFloat(name: "Verticle", 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            anim.SetFloat("Horizontal", -speed);
+
+
+        }
+        else if (Input.GetKey("d"))
+        {
+            anim.SetFloat("Horizontal", speed);
+
+
+        }
+        else
+        {
+            anim.SetFloat("Horizontal", 0);
 
 
         }
 
+        if (Input.GetKey("shift"))
+        {
+            anim.SetBool("Sprinting", true);
 
-        
+        }
+
+
     }
 }
